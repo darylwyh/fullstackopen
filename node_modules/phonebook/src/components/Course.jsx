2ec2656937eb7750d93a98f3520a2e96c8b0a060
darylwyh <wongyihern321@outlook.com>
2024-09-03 13:57:02 +0800

    2.5 done

--- node_modules/phonebook/src/components/Course.jsx
+++ node_modules/phonebook/src/components/Course.jsx
@@ -17,3 +17,10 @@ const Course = (props) => {
 
 
 export default Course
+
+/*
+2.5: Separate module step 10
+Declare the Course component as a separate module, 
+which is imported by the App component. 
+You can include all subcomponents of the course in the same module.
+*/
