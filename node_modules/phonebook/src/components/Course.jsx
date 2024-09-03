e5bc93f9d0d70a983e101ec4646867f3bb6eecbb
darylwyh <wongyihern321@outlook.com>
2024-09-03 13:53:37 +0800

    2.2 done

--- node_modules/phonebook/src/components/Course.jsx
+++ node_modules/phonebook/src/components/Course.jsx
@@ -2,12 +2,15 @@ import Content from './Content'
 import Header from './Header'
  
 const Course = (props) => {
-    console.log(props)
+    console.log(props) 
     const { course } = props
+    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+ 
     return (
       <div>
         <Header title={course.name} />
         <Content parts={course.parts} />
+        <p><strong>total of {totalExercises} exercises</strong></p>
       </div>
     )
   }
